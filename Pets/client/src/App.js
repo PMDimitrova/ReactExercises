ae5360fa5ddf0c372b14628edafc7c02cd740a22
PMDimitrova <palmina.dimitrova@gmail.com>
2021-07-26 12:01:16 +0300

    some minor changes

--- Pets/client/src/App.js
+++ Pets/client/src/App.js
@@ -3,17 +3,19 @@ import {Route, Switch} from 'react-router-dom';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Categories from "./components/Categories/Categories";
+import PetDetails from "./components/PetDetails/PetDetails";
 import './App.css';
 
 function App() {
     return (
         <div className="container">
-            <h1>Hello World</h1>
+            <h1>Hello World//to be edited</h1>
         <Header />
 
         <Switch>
             <Route path="/" exact component={Categories}/>
             <Route path="/categories/:category" component={Categories}/>
+            <Route path="/pets/details/:petId" component={PetDetails}/>
         </Switch>
 
         <Footer/>
--- Pets/client/src/components/Categories/Categories.js
+++ Pets/client/src/components/Categories/Categories.js
@@ -1,65 +1,98 @@
 import CategoryNavigation from "./CategoriesNavigation/CategoryNavigation";
+import Pet from "../Pet/Pet";
+import * as petsService from "../../services/petService";
 
-const Categories = ({
-            match
-                    }) => {
-    return (
-        <section className="dashboard">
-            <h1>Dashboard</h1>
+import {Component} from "react";
 
-            <CategoryNavigation />
+// class Categories extends Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             pets: [],
+//             currentCategory: 'all',
+//         }
+//     }
+//
+//     componentDidMount() {
+//         petsService.getAll()
+//             .then(res => this.setState({ pets: res }));
+//     }
+//
+//     componentDidUpdate(prevProps) {
+//         const category = this.props.match.params.category;
+//
+//         if (prevProps.match.params.category == category) {
+//             return;
+//         }
+//
+//         petsService.getAll(category)
+//             .then(res => {
+//                 this.setState({ pets: res, currentCategory: category })
+//             })
+//     }
+//
+//     render() {
+//         return (
+//             <div className="dashboard">
+//                 <h1>Dashboard</h1>
+//
+//                 <CategoryNavigation />
+//
+//                 <ul className="other-pets-list">
+//                     {this.state.pets.map(x =>
+//                         <p>{x.id}</p>
+//                         // <Pet key={x.id} {...x} />
+//                     )}
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
 
-            <ul className="other-pets-list">
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png"/></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#">
-                            <button className="button"><i className="fas fa-heart"></i> Pet</button>
-                        </a>
-                        <a href="#">
-                            <button className="button">Details</button>
-                        </a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png"/></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#">
-                            <button className="button"><i className="fas fa-heart"></i> Pet</button>
-                        </a>
-                        <a href="#">
-                            <button className="button">Details</button>
-                        </a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
+class Categories extends Component {
+    constructor(props) {
+        super(props);
 
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Kiro</h3>
-                    <p>Category: Dog</p>
-                    <p className="img"><img src="http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bbde.png"/>
-                    </p>
-                    <p className="description">This is my dog Kiro</p>
-                    <div className="pet-info">
-                        <a href="#">
-                            <button className="button"><i className="fas fa-heart"></i> Pet</button>
-                        </a>
-                        <a href="#">
-                            <button className="button">Details</button>
-                        </a>
-                        <i className="fas fa-heart"></i> <span> 4</span>
-                    </div>
-                </li>
-            </ul>
-        </section>
-    );
+        this.state = {
+            pets:[],
+            currentCategory: 'all'
+        }
+    }
+
+    componentDidMount() {
+        petsService.getAll()
+            .then(res => this.setState({pets: res}));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const category = this.props.match.params.category;
+        if(prevProps.match.params.category == category){
+            return;
+        }
+        petsService.getAll(category)
+            .then(res => {
+                this.setState({pets: res, currentCategory: category})
+            });
+    }
+
+    render() {
+        return (
+            <div className="dashboard">
+                <h1>Dashboard</h1>
+
+                <CategoryNavigation/>
+
+                <ul className="other-pets-list">
+                    {this.state.pets.map(x =>
+                        <Pet
+                            key={x.id}
+                            {...x}
+                        />)}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default Categories;
